e8e1e22f9f3ff37c4ae041bfae62a49c692d4459
TAGHRdooleyp <pjldooley@gmail.com>
2021-01-27 11:03:01 -0500

    qq

--- env/init.cmd
+++ env/init.cmd
@@ -3,6 +3,7 @@
 
 CALL LOG_ENTER \%DIR_TMPL%\env\init
 
-xcopy /q /y .\%DIR_TMPL%\env\win\*.cmd .\bin >NUL
+xcopy /q /y .\%DIR_TMPL%\env\win\pENV.cmd .\bin >NUL
+xcopy /q /y .\%DIR_TMPL%\env\win\pENV_MOD.sample.cmd .\project >NUL
 
 CALL LOG_EXIT \%DIR_TMPL%\env\init
