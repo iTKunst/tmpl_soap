bc296eb188ac89c02fb0dfc35fceaf4ddd1f3782
TAGHRdooleyp <pjldooley@gmail.com>
2021-01-27 11:21:29 -0500

    qq

--- env/init.cmd
+++ env/init.cmd
@@ -4,6 +4,11 @@
 CALL LOG_ENTER \%DIR_TMPL%\env\init
 
 xcopy /q /y .\%DIR_TMPL%\env\win\pENV.cmd .\bin >NUL
-xcopy /q /y .\%DIR_TMPL%\env\win\pENV_MOD.sample.cmd .\project >NUL
+
+if not exist project\env\win\pENV_MOD.cmd (
+  xcopy /q /y ^
+       %DIR_TMPL%\env\win\pENV_MOD.sample.cmd ^
+       project\env\win\pENV_MOD.cmd
+)
 
 CALL LOG_EXIT \%DIR_TMPL%\env\init
--- env/init.sh
+++ env/init.sh
@@ -4,6 +4,12 @@
 
 log_enter /$DIR_TMPL/env/init
 
-cp ./$DIR_TMPL/env/linux/*.sh ./bin
+cp ./$DIR_TMPL/env/linux/pENV.sh ./bin
+
+if [ ! -f project/env/linux/pENV_MOD.sh ]; then
+  xcopy /q /y \
+     $DIR_TMPL/env/linux/pENV.sh \
+     project/env/linux/pENV_MOD.sh
+fi
 
 log_exit /$DIR_TMPL/env/init
