c87d3c9fb3b424e2e786c614cb13d10e8d30854b
TAGHRdooleyp <pjldooley@gmail.com>
2021-02-09 18:49:33 -0500

    aa

---
+++ docker/linux/pBUILD.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# shellcheck disable=SC2086
+# echo pBUILD.sh [LOAD]
+
+source settings.sh
+source LOG.sh
+
+
+log_enter pBUILD
+
+
+source pENV.sh
+
+CONT_DIR=$PROJ_CONT_DIR
+HOST_DIR=$PROJ_HOST_DIR
+IMG=$PROJ_IMG
+
+CONT_CFG_DIR=$PROJ_CONT_CFG_DIR
+HOST_CFG_DIR=$PROJ_HOST_CFG_DIR
+USER=$PROJ_USER
+
+log_var CONT_DIR $CONT_DIR
+log_var DCKR_URI $DCKR_URI
+log_var HOST_DIR $HOST_DIR
+log_var IMG $IMG
+log_var DIR_SYS $DIR_SYS
+log_var TMPL_NAME $TMPL_NAME
+
+log_var CONT_CFG_DIR $CONT_CFG_DIR
+log_var HOST_CFG_DIR $HOST_CFG_DIR
+log_var MVN_REPO_IP $MVN_REPO_IP
+log_var MVN_REPO_URI $MVN_REPO_URI
+log_var USER $USER
+
+DOCKER_BUILDKIT=1 \
+          docker  \
+          build \
+          --no-cache \
+          --build-arg CONT_DIR=$CONT_DIR \
+          --build-arg HOST_CFG_DIR=$HOST_CFG_DIR \
+          --build-arg HOST_DIR=$HOST_DIR \
+          --build-arg SYS_DIR=$DIR_SYS \
+          --add-host=$MVN_REPO_URI:$MVN_REPO_IP \
+          -f $DCKR_URI \
+          -t $IMG \
+          .
+
+log_var RES $?
+if [ $? -ne 0 ]; then
+  log_err Unable to build image
+else
+  log_cmd Please run pGO to create and run the container
+fi
+
+log_exit pBUILD
+
+
+# echo pBUILD.sh [UNLOAD]
