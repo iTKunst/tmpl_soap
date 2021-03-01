e856dc54316dbaf2c6e347987937797d4577b2f7
TAGHRdooleyp <pjldooley@gmail.com>
2021-03-01 14:53:02 -0500

    aa

--- docker/linux/pINSTALL.sh
+++ docker/linux/pINSTALL.sh
@@ -15,7 +15,7 @@ log_var PROJ_NAME $PROJ_NAME
 log_var SYS_DIR $SYS_DIR
 log_var SYS_NAME $SYS_NAME
 
-export TARGET=../$SYS_DIR/soap/$PROJ_NAME
+export TARGET=../$SYS_NAME$SYS_DIR/soap/$PROJ_NAME
 export SOURCE=./target/generated-sources/axis2
 
 log_var TARGET $TARGET
